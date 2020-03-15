c7433937bd8f8e46f4a461e76764093281c5b919
Freddy <173239@ids.upchiapas.edu.mx>
2020-03-14 23:55:16 -0600

    cambio de url local a remota

--- app/src/main/java/com/example/myfragmentmvp/Helpers/Helpers.java
+++ app/src/main/java/com/example/myfragmentmvp/Helpers/Helpers.java
@@ -1,7 +1,7 @@
 package com.example.myfragmentmvp.Helpers;
 
 public class Helpers {
-    public static String URL = "http://192.168.0.11:3333/";
+    public static String URL = "http://ec2-3-87-170-106.compute-1.amazonaws.com:8000/";
     public static String token = "asd";
     public static boolean admin = false;
 }
